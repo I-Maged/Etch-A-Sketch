b27a5ac50759af973c0d9cf1254a83c29ee90ad7
I-Maged <magedhanafy@gmail.com>
2022-09-14 06:13:40 +0200

    Added color picker

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 let palette = document.querySelector('.palette');
 let header = document.querySelector('.header');
+let colorPicker = document.querySelector('.color-picker');
 
-palette.style.gridTemplateColumns = `repeat(${5} , 1fr)`;
-palette.style.gridTemplateRows = `repeat(${9} , 1fr)`;
+palette.style.gridTemplateColumns = `repeat(${50} , 1fr)`;
+palette.style.gridTemplateRows = `repeat(${50} , 1fr)`;
 
 console.log('Hello Etch -A-Sketch');
 
-for (let i = 0; i < 45; i++) {
+for (let i = 0; i < 2500; i++) {
   let square = document.createElement('div');
   square.style.backgroundColor = 'green';
-  square.style.border = '1px solid black';
-  palette.insertAdjacentElement('beforeend', square);
+  square.style.border = '1px solid #cccccc';
+  palette.appendChild(square);
 }
+
+colorPicker.addEventListener('input', (e) => {
+  console.log(e.target.value);
+});
